7eafec3c3fa76a45637e79ff91e436690b47c0e9
Egor Zholnin <e.zholnin@brightlab.me>
2025-02-18 18:25:41 +0300

    update data

--- src/orders/orders.gateway.ts
+++ src/orders/orders.gateway.ts
@@ -9,7 +9,7 @@ import { Server, Socket } from 'socket.io';
 import { OrdersService } from './orders.service';
 import { OrderStatus } from '@prisma/client';
 
-@WebSocketGateway(3000, { cors: true })
+@WebSocketGateway(8080, { cors: true })
 export class OrdersGateway {
   @WebSocketServer()
   server: Server;
